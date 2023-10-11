d19202908eddf07aabeb09a51b0e5e857f314ca0
giozarko <giodotjs@gmail.com>
2023-10-11 15:08:19 -0300

    adds static_root setting

--- config/settings.py
+++ config/settings.py
@@ -156,8 +156,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
