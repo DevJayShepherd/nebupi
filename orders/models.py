e99e0af0448c4c98f575455497e137459bc66075
giozarko <giodotjs@gmail.com>
2023-12-11 20:37:57 -0300

    remove unused model field

--- orders/models.py
+++ orders/models.py
@@ -24,7 +24,6 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    external_product_id = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
